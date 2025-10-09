ce0dc16c01418f1111843bf0dbbf61a3bd4ebed7
bhagwat <bhagwat.chate@bond.ai>
2025-10-09 14:56:24 +0530

    LLM load

--- research_and_analysis/utils/model_loader.py
+++ research_and_analysis/utils/model_loader.py
@@ -33,7 +33,7 @@ class ModelLoader:
         self.config = load_config()
 
         log.info(f"YAML config load", config_keys=list(self.config.keys()))
-        
+
     def load_embedding(self):
         try:
             model_name = self.config['embedding_model']['model_name']
@@ -53,3 +53,63 @@ class ModelLoader:
             log.error(f"Error loading embedding model: error: {e}")
             raise ResearchAnalysisException("Failed to load embedding model", sys)
         
+    def load_llm(self):
+        """
+        Load and return the configured LLM model.
+        """
+        llm_block = self.config["llm"]
+        provider_key = os.getenv("LLM_PROVIDER", "openai")
+
+        if provider_key not in llm_block:
+            log.error("LLM provider not found in config", provider=provider_key)
+            raise ValueError(f"LLM provider '{provider_key}' not found in config")
+
+        llm_config = llm_block[provider_key]
+        provider = llm_config.get("provider")
+        model_name = llm_config.get("model_name")
+        temperature = llm_config.get("temperature", 0.2)
+        max_tokens = llm_config.get("max_output_tokens", 2048)
+
+        log.info("Loading LLM", provider=provider, model=model_name)
+
+        if provider == "google":
+            return ChatGoogleGenerativeAI(
+                model=model_name,
+                google_api_key=self.api_key_mgr.get("GOOGLE_API_KEY"),
+                temperature=temperature,
+                max_output_tokens=max_tokens
+            )
+
+        elif provider == "groq":
+            return ChatGroq(
+                model=model_name,
+                api_key=self.api_key_mgr.get("GROQ_API_KEY"), #type: ignore
+                temperature=temperature,
+            )
+
+        elif provider == "openai":
+            return ChatOpenAI(
+                model=model_name,
+                api_key=self.api_key_mgr.get("OPENAI_API_KEY"),
+                temperature=temperature
+            )
+
+        else:
+            log.error("Unsupported LLM provider", provider=provider)
+            raise ValueError(f"Unsupported LLM provider: {provider}")
+
+if __name__=='__main__':
+    loader = ModelLoader()
+
+    embedding = loader.load_embedding()
+    print(f"embedding model laoded: {embedding}")
+
+    result = embedding.embed_query("Hello, how re you?")
+    print(f"embedding result: {result}")
+
+    llm = loader.load_llm()
+    print(f"LLM loaded: {llm}")
+
+    query = "Hi, how are you?"
+    result = llm.invoke(query)
+    print(f"Q: {query}\nA: {result.content}")
