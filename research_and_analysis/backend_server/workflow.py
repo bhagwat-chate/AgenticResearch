c10fa78a72e55bacd384ff4e12ef8b89439542d3
bhagwat <bhagwat.chate@bond.ai>
2025-10-28 17:20:39 +0530

    update

--- research_and_analysis/backend_server/workflow.py
+++ research_and_analysis/backend_server/workflow.py
@@ -16,6 +16,46 @@ from research_and_analysis.backend_server.models import Analyst, Perspectives, G
 from research_and_analysis.utils.model_loader import ModelLoader
 
 
+def build_interview_graph(llm, tavily_search=None):
+
+    memory = MemorySaver()
+
+    def generate_question(state: InterviewState):
+        pass
+
+    def search_web(state: InterviewState):
+        pass
+
+    def generate_answer(state: InterviewState):
+        pass
+
+    def save_interview(state: InterviewState):
+        pass
+
+    def write_section(state: InterviewState):
+        pass
+
+    builder = StateGraph(InterviewState)
+
+    builder.add_node('ask_question', generate_question)
+    builder.add_node('search_web', search_web)
+    # builder.add_node('search_wikipedia', search_wikipedia)
+    builder.add_node('generate_answer', generate_answer)
+    builder.add_node('save_interview', save_interview)
+    builder.add_node('write_section', write_section)
+
+    builder.add_edge(START, 'ask_question')
+    builder.add_edge('ask_question', 'search_web')
+    # builder.add_edge('ask_question', 'search_wikipedia')
+    builder.add_edge('search_web', 'generate_answer')
+    builder.add_edge('generate_answer', 'save_interview')
+    builder.add_edge('save_interview', 'write_section')
+    builder.add_edge('write_section', END)
+
+    return builder.compile(checkpointer=memory).with_config(run_name='Conduct Interview')
+    # display(Image(interview_graph.get_graph().draw_mermaid_png()))
+
+
 class AutonomousReportGeneration:
     def __init__(self):
         pass
@@ -52,4 +92,9 @@ class AutonomousReportGeneration:
 
 
 if __name__ == '__main__':
-    obj = AutonomousReportGeneration()
+    ml = ModelLoader()
+    llm = ml.load_llm()
+    print(llm.invoke('Hi').content)
+
+    reporter = AutonomousReportGeneration()
+    reporter.build_graph()
